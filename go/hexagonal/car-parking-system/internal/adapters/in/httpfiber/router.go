aef50beff89ee9ba7a3c0f91246c225848600c7b
anime454 <anime0856047129@gmail.com>
2026-03-31 17:18:02 +0700

    feat: implement routes

--- go/hexagonal/car-parking-system/internal/adapters/in/httpfiber/router.go
+++ go/hexagonal/car-parking-system/internal/adapters/in/httpfiber/router.go
@@ -1,1 +1,19 @@
 package httpfiber
+
+import "github.com/gofiber/fiber/v3"
+
+func registerRoutes(app *fiber.App, opts Options) {
+
+	// Health check that doesn't touch core
+	app.Get("/health", func(c fiber.Ctx) error {
+		return c.SendString("ok")
+	})
+
+	apiV1 := app.Group("/api/v1")
+
+	vehicle := apiV1.Group("/vehicles")
+
+	vehicle.Post("/in", func(c fiber.Ctx) error { return nil })
+	vehicle.Post("/out", func(c fiber.Ctx) error { return nil })
+
+}
